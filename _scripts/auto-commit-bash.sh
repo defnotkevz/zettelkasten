1d9dc99b265f514915df1a1763d1fe8f4aa969a8
kevinsourcode <kevnroy1@gmail.com>
2024-07-02 16:57:38 +0530

    added bash script for auto-commit

---
+++ _scripts/auto-commit-bash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+repo_path="~/personal/zettelkasten"
+
+cd "$repo_path" || exit
+
+changes=$(git status --porcelain "*.md")
+
+if [ -n "$changes" ]; then
+    git add .
+    git commit -m 'daily commit'
+    git push origin main
+    echo "Changes committed successfully"
+else
+    echo "No changes detected"
+fi
